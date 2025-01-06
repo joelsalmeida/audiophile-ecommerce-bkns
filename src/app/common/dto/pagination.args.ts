10d791fd830ea947256bfb305ea6fbaca5ba16d8
Joel Almeida <joel.almeida.sa@gmail.com>
2025-01-06 20:16:46 -0300

    feat: update PaginationArgs to make limit and skip fields required

--- src/app/common/dto/pagination.args.ts
+++ src/app/common/dto/pagination.args.ts
@@ -3,9 +3,9 @@ import { Field, ArgsType, Int, InputType } from '@nestjs/graphql';
 @InputType()
 @ArgsType()
 export class PaginationArgs {
-  @Field(() => Int, { defaultValue: 10, nullable: true })
-  limit?: number;
+  @Field(() => Int, { defaultValue: 10, nullable: false })
+  limit: number;
 
-  @Field(() => Int, { defaultValue: 0, nullable: true })
-  skip?: number;
+  @Field(() => Int, { defaultValue: 0, nullable: false })
+  skip: number;
 }
