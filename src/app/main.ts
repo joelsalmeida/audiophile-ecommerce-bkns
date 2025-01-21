cf831615793c41e20714d8e3b07e81000d0f7359
Joel Almeida <joel.almeida.sa@gmail.com>
2025-01-21 15:37:02 -0300

    feat: enable CORS with specific origin and update fallback server port to 4000

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const ALLOW_ORIGIN_HEADER = 'http://localhost:3000';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: ALLOW_ORIGIN_HEADER, credentials: true },
+  });
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
